ecda22001f3dcb7e0dd4f1b4d44b0d5496f65490
BHesseldieck <b.hesseldieck@gmail.com>
2016-10-10 15:57:22 -0700

    created user table

--- app/config.js
+++ app/config.js
@@ -17,6 +17,7 @@ db.knex.schema.hasTable('urls').then(function(exists) {
       link.string('code', 100);
       link.string('title', 255);
       link.integer('visits');
+      link.integer('user_id');
       link.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
@@ -39,6 +40,17 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
 /************************************************************/
 // Add additional schema definitions below
 /************************************************************/
-
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('username');
+      user.string('password');
+      user.string('salt');
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
 
 module.exports = db;
--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,24 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
+var Link = require('./link');
 var Promise = require('bluebird');
 
 
 
 var User = db.Model.extend({
+  tableName: 'users',
+
+  links: function() {
+    return this.hasMany(Link);
+  },
+  initialize: function() {
+    this.on('createUser', function(model, attrs, options) {
+      var shasum = bcrypt.hash('sha1');
+      shasum.update(model.get('url'));
+      model.set('code', shasum.digest('hex').slice(0, 5));
+    });
+  }
+  // change password function
 });
 
 module.exports = User;
